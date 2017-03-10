4e7437205866f45dc8c6c266e2b809c3eb3c2780
ancjf <ancjf@163.com>
2017-03-10 15:10:12 +0800

    Test no problem！

--- bakery/bakery.cpp
+++ bakery/bakery.cpp
@@ -19,8 +19,8 @@ int number[N];
 
 int max_number(int *num, unsigned len)
 {
-	int ret = 0;
-	for(unsigned i = 0; i < len; i++){
+	int ret = num[0];
+	for(unsigned i = 1; i < len; i++){
 		if(num[i] > ret)
 			ret = num[i];
 	}
@@ -28,14 +28,26 @@ int max_number(int *num, unsigned len)
 	return ret;
 }
 
-int g_process_number = 0;
+bool number_bigger(int ticket1, int n1, int ticket2, int n2)
+{
+	if(ticket1 > ticket2)
+		return true;
+
+	if(ticket1 == ticket2 && n1 > n2)
+		return true;
+
+	return false;
+}
+
+#define wait_until(X) do{while(!(X)){Sleep(1);}assert(X);}while(false)
+
+volatile int g_process_number = 0;
 volatile int g_rand;
 
 void process(void* arg)
 {
-	srand(GetTickCount());
-
 	unsigned i = (unsigned)arg;
+	srand(GetTickCount()+i);
 
 	do{
 		choosing[i] = true;
@@ -45,32 +57,41 @@ void process(void* arg)
 			if(i == j)
 				continue;
 
-			while (choosing[j]) {//保证编号较小的进程先进入临界区
-				// wait until process j receives its number
-				Sleep(1);
-			}
-
-			while ((number[j]!=0) && !(number[j] > number[i] || j > i)) { //进程Pj是其他线程
-				Sleep(1);
-				// wait until processes with smaller numbers
-				// or with the same number, but with higher
-				// priority, finish their work
-			}
+			wait_until(choosing[j] == false);
+			//保证编号较小的进程先进入临界区
+			// wait until process j receives its number
+			
+			wait_until(number[j] == 0 || number_bigger(number[j], j, number[i], i));
+			
+			//进程Pj是其他线程
+			// wait until processes with smaller numbers
+			// or with the same number, but with higher
+			// priority, finish their work
 	     }
 
 		int r = rand();
+		//if(g_rand)
+		printf("enter i = %u, number[%d] = %d, r = %d, g_rand = %d\r\n", i, i, number[i], r, g_rand);
+		assert(!g_rand);
+
 		g_rand = r;
 
-		for(int i = 0; i < 100; i++){
-			assert(g_rand == r);
+		for(int i = 0; i < 100; i++){	
 			if(g_rand != r)
 				printf("test err i = %u, r = %d, g_rand = %d\r\n", i, r, g_rand);
+
+			assert(g_rand == r);
 			Sleep(1);
 		}
 
 		printf("i = %u, r = %d, g_rand = %d\r\n", i, r, g_rand);
+
+		
 		g_process_number++;
 	     // critical section...
+
+		printf("leave i = %u, number[%d] = %d, r = %d, g_rand = %d\r\n", i, i, number[i], r, g_rand);
+		g_rand = 0;
 		number[i] = 0;
 	     // non-critical section...
 	}while (false);
@@ -78,6 +99,11 @@ void process(void* arg)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	for(unsigned i = 0; i < N; i++){
+		choosing[i] = false;
+		number[i] = 0;
+	}
+
 	for(unsigned i = 0; i < N; i++){
 		_beginthread(process, 0, (void*)i);
 	}
